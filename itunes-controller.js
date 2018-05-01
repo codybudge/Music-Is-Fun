f9aa0c5b26e2516f05fe3d6d16501ab8b595f1e2
cody budge <codybudge@gmail.com>
2018-05-01 14:32:23 -0600

    codys fun music

--- itunes-controller.js
+++ itunes-controller.js
@@ -9,6 +9,26 @@ function ItunesController(){
 
   //Start coding here
   function draw(results){
+    var template = '';
+    for (var i = 0; i < results.length; i++) {
+      var result = results[i];
+      template += `
+      <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${result.artworkUrl60}" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">${result.trackName}</h5>
+              <p class="card-text">${result.artistName}</p>
+              <p class="card-text">${result.collectionName}</p>
+              <p class="card-text">${result.collectionPrice}</p>
+              <audio controls="controls">
+                <source src="${result.previewUrl}" type="audio/wav">
+              <a href="#" class="btn btn-primary">Play</a>
+            </div>
+          </div>
+      `
+    }
+    var songElm = document.getElementById('songs');
+    songElm = innerHTML = template;
     console.log(results)
   }
 
